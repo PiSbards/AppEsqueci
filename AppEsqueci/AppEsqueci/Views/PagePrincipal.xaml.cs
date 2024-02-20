3e5ec5e17523c208d9643871b45da35aeb908d11
PiSbards <psbardellati@hotmail.com>
2024-02-19 22:12:35 -0300

    Atualização do programa

--- AppEsqueci/AppEsqueci/Views/PagePrincipal.xaml.cs
+++ AppEsqueci/AppEsqueci/Views/PagePrincipal.xaml.cs
@@ -10,11 +10,36 @@ using Xamarin.Forms.Xaml;
 namespace AppEsqueci.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class PagePrincipal : ContentPage
+    public partial class PagePrincipal : MasterDetailPage
     {
         public PagePrincipal()
         {
             InitializeComponent();
+            btHome_Clicked(new Object(),new EventArgs());
+        }
+
+        private void btHome_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new PageHome());
+            IsPresented = false;
+        }
+
+        private void btCadastrar_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new PageCadastrar());
+            IsPresented = false;
+        }
+
+        private void btLocalizar_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new PageListar());
+            IsPresented = false;
+        }
+
+        private void btSobre_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new PageSobre());
+            IsPresented = false;
         }
     }
 }
