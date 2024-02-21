3af661efaf5506c909ad7f1107bfe2847df77490
PiSbards <psbardellati@hotmail.com>
2024-02-20 21:14:12 -0300

    Att 20.02.2024

--- AppEsqueci/AppEsqueci/Views/PageCadastrar.xaml.cs
+++ AppEsqueci/AppEsqueci/Views/PageCadastrar.xaml.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using AppEsqueci.Model;
+using AppEsqueci.Services;
+
 namespace AppEsqueci.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +18,67 @@ namespace AppEsqueci.Views
         public PageCadastrar()
         {
             InitializeComponent();
+        }
+
+        public PageCadastrar(ModelNotas nota)
+        {
+            InitializeComponent();
+            btSalvar.Text = "Alterar";
+            txtCodigo.IsVisible = true;
+            btExcluir.IsVisible = true;
+            txtCodigo.Text = nota.Id.ToString();
+            txtTitulo.Text = nota.Titulo;
+            txtDados.Text = nota.Dados;
+            swFavorito.IsToggled = nota.Favorito;
+        }
+
+        private void btSalvar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                ModelNotas notas = new ModelNotas();
+                notas.Titulo = txtTitulo.Text;
+                notas.Dados = txtDados.Text;
+                notas.Favorito = swFavorito.IsToggled;
+                ServiceDBNotas dbNotas = new ServiceDBNotas(App.DbPath);
+                if (btSalvar.Text == "Inserir")
+                {
+                    dbNotas.Inserir(notas);
+                    DisplayAlert("Resultado", dbNotas.StatusMessage, "OK");
+                }
+                else
+                {
+                    notas.Id = Convert.ToInt32(txtCodigo.Text);
+                    dbNotas.Alterar(notas);
+                    DisplayAlert("Nota Alterada com Sucesso!", dbNotas.StatusMessage, "OK");
+                }
+                MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+                p.Detail = new NavigationPage(new PageHome());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void btExcluir_Clicked(object sender, EventArgs e)
+        {
+            var resp = await DisplayAlert("Excluir nota","Deseja excluir a nota selecionada?","Sim","Não");
+            if(resp == true)
+            {
+                ServiceDBNotas dbNotas = new ServiceDBNotas(App.DbPath);
+                int id = Convert.ToInt32(txtCodigo.Text);
+                dbNotas.Excluir(id);
+                DisplayAlert("Nota excluída com Sucesso!", dbNotas.StatusMessage,"OK");
+                MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+                p.Detail = new NavigationPage(new PageHome());
+            }
+        }
+
+        private void btCancelar_Clicked(object sender, EventArgs e)
+        {
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new PageHome());
         }
     }
 }
--- AppEsqueci/AppEsqueci/Views/PageListar.xaml.cs
+++ AppEsqueci/AppEsqueci/Views/PageListar.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using AppEsqueci.Model;
+using AppEsqueci.Services;
 
 namespace AppEsqueci.Views
 {
@@ -15,6 +17,39 @@ namespace AppEsqueci.Views
         public PageListar()
         {
             InitializeComponent();
+            atualizaLista();
+        }
+
+        public void atualizaLista()
+        {
+            string titulo = "";
+            if (txtNota.Text != null) titulo = txtNota.Text;            
+            ServiceDBNotas dbNotas = new ServiceDBNotas(App.DbPath);
+            if (swFavoritos.IsToggled)
+            {
+                ListaNotas.ItemsSource = dbNotas.Localizar(titulo, true);
+            }
+            else
+            {
+                ListaNotas.ItemsSource = dbNotas.Localizar(titulo);
+            }
+        }
+
+        private void swFavoritos_Toggled(object sender, ToggledEventArgs e)
+        {
+            atualizaLista();
+        }
+
+        private void btLocalizar_Clicked(object sender, EventArgs e)
+        {
+            atualizaLista();
+        }
+
+        private void ListaNotas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ModelNotas nota = (ModelNotas)ListaNotas.SelectedItem;
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new PageCadastrar(nota));
         }
     }
 }
